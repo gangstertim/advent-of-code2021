f56e5d1af15307a78d9dfdc0a5a80c298752dc3f
Tim Miller <hungryhungrytimo@gmail.com>
2021-12-06 05:33:00 +0000

    finished 6

--- 6/solve.js
+++ 6/solve.js
@@ -1,11 +1,60 @@
 const { parseInput } = require('../parseInput');
 
-function solve(data) {
-  return data;
+function solve(fish) {
+  for (let i=0; i<128; i++) {
+    fish.forEach((f, i) => {
+      if(f) {
+        fish[i] = f-1;
+      } else {
+        fish[i] = 6;
+        fish.push(8);
+      } 
+    });
+  }
+  console.log(fish.length);
+}
+
+function solve2(fish) {
+  const memo = [];
+  for (let j=0; j<=8; j++) {
+    let protoFish = [j];
+
+    for (let i=0; i<128; i++) {
+      protoFish.forEach((f, i) => {
+        if(f) {
+          protoFish[i] = f-1;
+        } else {
+          protoFish[i] = 6;
+          protoFish.push(8);
+        } 
+      });
+    }
+    memo[j] = protoFish.length;
+  }
+
+  for (let i=0; i<128; i++) {
+    fish.forEach((f, i) => {
+      if(f) {
+        fish[i] = f-1;
+      } else {
+        fish[i] = 6;
+        fish.push(8);
+      } 
+    });
+  }
+
+  console.log(memo);
+  let total = 0;
+  fish.forEach(f => {
+    total+=memo[f];
+  });
+  console.log(total);
 }
 
 const data = parseInput('./6/input.txt');
-const solution = solve(data);
+const fish = [];
+data[0].split(',').forEach(el => fish.push(parseInt(el)));
+const solution = solve2(fish);
 
 module.exports = {
 	solution,
